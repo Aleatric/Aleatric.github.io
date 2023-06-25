33da14ae6ae887d8cfd8e526a4cf405cb50daca0
Aleatric <117276468+Aleatric@users.noreply.github.com>
2023-06-25 19:58:02 +1000

    Update myscripts.js
    big boy update

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,3 @@
-const scrollUp = document.querySelector("#scroll-up");
-
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
-
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
